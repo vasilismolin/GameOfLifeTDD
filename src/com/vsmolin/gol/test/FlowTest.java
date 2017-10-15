ffa3d2ba79f44386a18c8cde8f4e3f88229e664f
vasilismolin <32807634+vasilismolin@users.noreply.github.com>
2017-10-15 10:48:15 -0400

    fixed flow tests

--- src/com/vsmolin/gol/test/FlowTest.java
+++ src/com/vsmolin/gol/test/FlowTest.java
@@ -21,6 +21,7 @@ public class FlowTest
         testFlow.start();
         try
         {
+            int a = 3;
             Thread.sleep(waitTime);
         } catch (Exception e){}
         testFlow.stop();
